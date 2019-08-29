3404f0d982730c4013009d8ec136b555105a8461
Keiichiro Ono <kono@ucsd.edu>
2019-08-29 13:45:44 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='auto-graph-visualizer',
+    version='0.1.0',
+    description='Automatic graph visualization package',
+    long_description=readme,
+    author='',
+    author_email='',
+    url='https://github.com/idekerlab/auto-graph-visualizer',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
